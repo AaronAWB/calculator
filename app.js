b319591cec2fcfc02f0512cf2d34925a7da16d29
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2022-09-12 21:49:32 -0700

    add selector variables

---
+++ app.js
@@ -0,0 +1,5 @@
+const display = document.querySelector(".display");
+const numberButtons = document.querySelectorAll(".number-button");
+const operatorButtons = document.querySelectorAll(".operator-button");
+const memoryButtons = document.querySelectorAll(".memory-button");
+const memoryDisplay = document.querySelector(".memory-display");
