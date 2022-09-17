d0ff364853ae8ad7e6ae54ec475fa6d3918f21ac
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2022-09-17 12:40:26 -0700

    revise button classes

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
-const display = document.querySelector(".display");
-const numberButtons = document.querySelectorAll(".number-button");
-const operatorButtons = document.querySelectorAll(".operator-button");
-const memoryButtons = document.querySelectorAll(".memory-button");
+const display = document.querySelector('.display');
+
+const numberButtons = document.querySelectorAll('.number-button');
+const operatorButtons = document.querySelectorAll('.operator-button');
+const allClearButton = document.querySelector('.all-clear-button');
+const deleteButton = document.querySelector('.delete-button');
+const equalsButton = document.querySelector('.equals-button');
+
+const memoryAddButton = document.querySelector(".memory-add-button");
+const memoryRemoveButton = document.querySelector(".memory-remove-button");
+const memoryRecallButton = document.querySelector(".memory-recall-button");
+const memoryClearButton = document.querySelector(".memory-clear-button");
 const memoryDisplay = document.querySelector(".memory-display");
+
+numberButtons.forEach (function(button){
+    button.addEventListener ('click', function() {
+        const ButtonValue = button.innerText;
+        displayInfo(ButtonValue)
+    })
+})
+
+function displayInfo () {
+    console.log('clicked')
+    console.log(buttonValue);
+    console.log(buttonValue.target);
+    console.log(buttonValue.target.innerText);
+}
