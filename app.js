2ca2156c15bb05a512df7f9ded2b332fe82e8a96
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2022-09-23 12:03:47 -0700

    add allClear function

--- app.js
+++ app.js
@@ -26,6 +26,8 @@ operatorButtons.forEach(function(button){
     })
 })
 
+allClearButton.addEventListener('click', allClear);
+
 function updatePrimaryDisplay() {
     const primaryDisplay = document.querySelector('.primary-display');
     primaryDisplay.innerText = currentOperand;
@@ -48,14 +50,18 @@ function inputNumber(number) {
 function inputOperator(selectedOperator) {
     operator = selectedOperator;
     updatePrimaryDisplay();
-    updateSecondaryDisplay();
+    updateSecondaryDisplay('calculation');
 }
 
-function updateSecondaryDisplay() {
+function updateSecondaryDisplay(operation) {
     const secondaryDisplay = document.querySelector('.secondary-display');
-    previousOperand = currentOperand;
-    secondaryDisplay.innerText = previousOperand + " " + operator;
-    clearCurrentOperand();
+    if (operation === 'calculation') {
+        previousOperand = currentOperand;
+        secondaryDisplay.innerText = previousOperand + " " + operator;
+        clearCurrentOperand();
+    } else {
+        secondaryDisplay.innerText = ''
+    }
 }
 
 function clearCurrentOperand() {
@@ -89,142 +95,10 @@ function performCalculation(currentOperand, previousOperand, operator) {
     return result.toString();
 }
 
-
-
-
-
-
-
-
-
-
-
-
-// allClearButton.addEventListener('click', clearDisplay);
-// backSpaceButton.addEventListener('click', deleteLastDigit);
-// equalsButton.addEventListener('click', displayResult);
-
-// function updateDisplay() {
-//     const display = document.querySelector('.display');
-//     display.innerText = displayValue
-//     if (displayValue.length > 10) {
-//         displayValue = displayValue.substring (0, 10);
-//     }
-// }
-
-// updateDisplay();
-
-// function inputOperand(operand) {
-//     if (displayState === null) {
-//         operandOne = operand;
-//         displayValue = operandOne;
-//         updateDisplayState('operandOne');
-//     } else if (displayState === 'operandOne' && !displayHasOperator) {
-//         operandOne += operand;
-//         displayValue = operandOne;
-//     } else if (displayState === 'operandOne' && displayHasOperator) {
-//         updateDisplayState('operandTwo');
-//         operandTwo = operand;
-//         displayValue = operandTwo;
-//     } else if (displayState === 'operandTwo') {
-//         operandTwo += operand;
-//         displayValue = operandTwo;
-//     } else if (displayState === 'result' && displayHasOperator) {
-//         operandTwo += operand;
-//         displayValue = operandTwo;
-//         updateDisplayState('operandTwo');
-//     }
-//     updateDisplay(); 
-// }
-
-// function updateDisplayState(operandState) {
-//     switch (operandState) {
-//         case 'operandOne': displayState = 'operandOne';
-//             break;
-//         case 'operandTwo': displayState = 'operandTwo';
-//             break;
-//         case 'result': displayState = 'result';
-//     }
-// }
-
-// function inputOperator(operator) {
-//     if (operandOne != null && operandTwo === null) {
-//         storedOperator = operator;
-//         displayHasOperator = true; 
-//     } else if (result != null && operandTwo === null) {
-//         storedOperator = operator;
-//         displayHasOperator = true;
-//     }
-//     // updateOperatorButton();
-// }
-
-// function updateOperatorButton(event) {
-//     const selectedOperatorButton = event.target;
-//     selectedOperatorButton.classList.toggle("selected");
-//     // Code to add highlighted class to selected operator button.
-// }
-
-// function performCalculation(operandOne, operandTwo, storedOperator) {
-//     if (operandOne != null && operandTwo != null) {
-//         switch (storedOperator) {
-
-//             case '/':
-//                  return operandOne / operandTwo;
-//             case '*':
-//                 return operandOne * operandTwo;
-//             case '+':
-//                 return parseInt(operandOne) + parseInt(operandTwo);
-//             case '-':
-//                 return operandOne - operandTwo;
-//         }
-//     }
-// }
-
-// function displayResult() {
-//     if (result === null) {
-//         let calculation = performCalculation(operandOne, operandTwo, storedOperator);
-//         result = calculation.toString();
-//         displayValue = result;
-//         updateDisplay();
-//         updateDisplayState(result);
-//         displayHasOperator = false;
-//         displayedOperator = null;
-//         operandTwo = null;
-//         operandOne = null;
-//     } else {
-//         let calculation = performCalculation(result, operandTwo, storedOperator);
-//         result = calculation.toString();
-//         displayValue = result;
-//         updateDisplay();
-//         updateDisplayState(result);
-//         displayHasOperator = false;
-//         diplayedOperator = null;
-//         operandTwo = null;
-//         operandOne = null;
-//     }
-// }
-
-// function clearDisplay() {
-//     displayValue = 0;
-//     operandOne = null;
-//     operandTwo = null;
-//     displayedOperator = null;
-//     displayState = null;
-//     displayHasOperator = false;
-//     result = null;
-//     updateDisplay();
-// }
-
-// function deleteLastDigit() {
-//     displayValue = displayValue.slice(0, -1);
-// }
-
-// function showValues() {
-//     console.log(`OperandOne = ${operandOne}`);
-//     console.log(`OperandTwo = ${operandTwo}`);
-//     console.log(`storedOperator = ${storedOperator}`);
-//     console.log(`displayState = ${displayState}`);
-//     console.log(`displayValue = ${displayValue}`);
-//     console.log(`displayHasOperator = ${displayHasOperator}`);
-//     console.log(`result = ${result}`);
-// }
+function allClear() {
+    currentOperand = '0';
+    previousOperand = ''
+    operator = '';
+    updateSecondaryDisplay();
+    updatePrimaryDisplay();
+}
