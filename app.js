0824f6e79f3669e038f75cbfcd57b3be4affb2d0
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2022-09-23 11:38:09 -0700

    refactor all functions for dual display

--- app.js
+++ app.js
@@ -17,14 +17,12 @@ const memoryDisplay = document.querySelector(".memory-display");
 numberButtons.forEach(function(button){
     button.addEventListener('click', function(){
         inputNumber(button.innerText);
-        updatePrimaryDisplay();
     })
 })
 
 operatorButtons.forEach(function(button){
     button.addEventListener('click', function(){
         inputOperator(button.innerText);
-        // updateOperands();
     })
 })
 
@@ -40,24 +38,55 @@ updatePrimaryDisplay();
 
 function inputNumber(number) {
     if (!operator) {
-    currentOperand === '0' ? currentOperand = number.toString() : currentOperand += number.toString();
+        currentOperand === '0' ? currentOperand = number.toString() : currentOperand += number.toString();
     } else {
-        previousOperand = currentOperand;
-        updateSecondaryDisplay(previousOperand);
-        operator = ''
-        currentOperand = number;
+        currentOperand += number.toString();
     }
+    updatePrimaryDisplay();
 }
 
 function inputOperator(selectedOperator) {
     operator = selectedOperator;
-    currentOperand = currentOperand + " " + operator;
+    updatePrimaryDisplay();
+    updateSecondaryDisplay();
+}
+
+function updateSecondaryDisplay() {
+    const secondaryDisplay = document.querySelector('.secondary-display');
+    previousOperand = currentOperand;
+    secondaryDisplay.innerText = previousOperand + " " + operator;
+    clearCurrentOperand();
+}
+
+function clearCurrentOperand() {
+    currentOperand = '';
     updatePrimaryDisplay();
 }
 
-function updateSecondaryDisplay(previousOperand) {
-    const secondaryDisplay = document.querySelector('.secondary-display');
-    secondaryDisplay.innerText = previousOperand;
+equalsButton.addEventListener('click', function() {
+   currentOperand = performCalculation(currentOperand, previousOperand, operator);
+   updatePrimaryDisplay();
+})
+
+function performCalculation(currentOperand, previousOperand, operator) {
+    let result = '';
+    if (currentOperand && previousOperand) {
+        switch (operator) {
+            case '/':
+                result = previousOperand / currentOperand;
+                break;
+            case '*':
+                result = previousOperand * currentOperand;
+                break;
+            case '+':
+                result = parseInt(previousOperand) + parseInt(currentOperand);
+                break;
+            case '-':
+                result = previousOperand - currentOperand;
+                break;
+        }
+    }
+    return result.toString();
 }
 
 
@@ -69,32 +98,7 @@ function updateSecondaryDisplay(previousOperand) {
 
 
 
-// const numberButtons = document.querySelectorAll('.number-button');
-// const operatorButtons = document.querySelectorAll('.operator-button');
-// const allClearButton = document.querySelector('.all-clear-button');
-// const backSpaceButton = document.querySelector('.backspace-button');
-// const equalsButton = document.querySelector('.equals-button');
 
-// const memoryAddButton = document.querySelector(".memory-add-button");
-// const memoryRemoveButton = document.querySelector(".memory-remove-button");
-// const memoryRecallButton = document.querySelector(".memory-recall-button");
-// const memoryClearButton = document.querySelector(".memory-clear-button");
-// const memoryDisplay = document.querySelector(".memory-display");
-
-// numberButtons.forEach(function(button){
-//     button.addEventListener('click', function(){
-//         inputOperand(button.innerText);
-//         updateDisplay();
-//     })
-// })
-
-// operatorButtons.forEach(function(button){
-//     button.addEventListener('click', function(){
-//         inputOperator(button.innerText);
-//         updateOperatorButton('click');
-//         updateDisplay();
-//     })
-// })
 
 // allClearButton.addEventListener('click', clearDisplay);
 // backSpaceButton.addEventListener('click', deleteLastDigit);
